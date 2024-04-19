3c9327f2a26e7f36039f80887731e52de80ce49a
khalilr14 <khalilramzy14@gmail.com>
2024-04-19 23:34:22 +0700

    tes commit regex coba

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 $(function ()
 { 
     const email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const handphone = /^\+?\d{6,15}$/;
-    const post = /^\d{5}$/;
-    const id = /^[A-Z]{3}\d{3}$/;
-    const pass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const notelepon = /^\+?\d{6,15}$/;
+    const username = /^[a-zA-Z0-9]{3,16}$/;
+    const ipv4 = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    const passw = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/;
 
     $('input').each(function ()
     {
@@ -16,73 +16,73 @@ $(function ()
             {
                 if (email.test($(this).val()))
                 {
-                    pElement.html('Email yang Anda input sudah Valid')
+                    pElement.html('Bagus, Email yang Anda input sudah VALID.')
                     pElement.removeClass('text-red-600')
                     pElement.addClass('text-green-600')
                 }
                 else
                 {
-                    pElement.html('Maaf Email yang Anda input Tidak Valid, coba lagi.')
+                    pElement.html('Maaf, Email yang Anda input TIDAK VALID, coba lagi.')
                     pElement.removeClass('text-green-600')
                     pElement.addClass('text-red-600')
                 }
             }
-            if ($(this).attr('id') == "handphone")
+            if ($(this).attr('id') == "notelepon")
             {
-                if (handphone.test($(this).val()))
+                if (notelepon.test($(this).val()))
                 {
-                    pElement.html('No. Handphone yang Anda input sudah Valid')
+                    pElement.html('Bagus, No. Telepon yang Anda input sudah VALID.')
                     pElement.removeClass('text-red-600')
                     pElement.addClass('text-green-600')
                 }
                 else
                 {
-                    pElement.html('No. Handphone yang Anda input Tidak Valid, coba lagi.')
+                    pElement.html('Maaf, No. Telepon yang Anda input TIDAK VALID, coba lagi.')
                     pElement.removeClass('text-green-600')
                     pElement.addClass('text-red-600')
                 }
             } 
-            if ($(this).attr('id') == "post")
+            if ($(this).attr('id') == "username")
             {
-                if (post.test($(this).val()))
+                if (username.test($(this).val()))
                 {
-                    pElement.html('No. Post yang Anda input sudah Valid')
+                    pElement.html('Bagus, Username yang Anda input sudah VALID.')
                     pElement.removeClass('text-red-600')
                     pElement.addClass('text-green-600')
                 }
                 else
                 {
-                    pElement.html('No. Post yang Anda input Tidak Valid, coba lagi.')
+                    pElement.html('Hanya huruf dan angka, minimal 3 karakter, maksimal 16 karakter.')
                     pElement.removeClass('text-green-600')
                     pElement.addClass('text-red-600')
                 }
             } 
-            if ($(this).attr('id') == "id")
+            if ($(this).attr('id') == "ipv4")
             {
-                if (id.test($(this).val()))
+                if (ipv4.test($(this).val()))
                 {
-                    pElement.html('No. Post yang Anda input sudah Valid')
+                    pElement.html('Bagus, IPv4 yang Anda input sudah VALID.')
                     pElement.removeClass('text-red-600')
                     pElement.addClass('text-green-600')
                 }
                 else
                 {
-                    pElement.html('Coba ABC333.')
+                    pElement.html('Contoh format IPv4: "192.168.0.1", "10.0.0.1", "172.16.0.1", "192.0.2.1", "127.0.0.1", "255.255.255.0"')
                     pElement.removeClass('text-green-600')
                     pElement.addClass('text-red-600')
                 }
             } 
-            if ($(this).attr('id') == "pass")
+            if ($(this).attr('id') == "passw")
             {
-                if (pass.test($(this).val()))
+                if (passw.test($(this).val()))
                 {
-                    pElement.html('Pass yang Anda input sudah Valid')
+                    pElement.html('Bagus, Password yang Anda input sudah VALID.')
                     pElement.removeClass('text-red-600')
                     pElement.addClass('text-green-600')
                 }
                 else
                 {
-                    pElement.html('Coba 1 angka 1 huruf besar 1 huruf.')
+                    pElement.html('Minimal 8 karakter, minimal satu huruf besar, satu huruf kecil, satu angka, dan satu karakter khusus.')
                     pElement.removeClass('text-green-600')
                     pElement.addClass('text-red-600')
                 }
